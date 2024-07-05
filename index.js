bbcdea13d549da26823fe1b7bc82e01f4b463334
Rabbi1089 <rabbi.ext@gmail.com>
2024-07-05 18:54:37 +0600

    add tab-based system for browsing jobs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 9000;
 
 const app = express()
@@ -14,6 +15,40 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t241ufd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    //await deleted
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // get data from jobcollection
+
+    const jobcollection = client.db('TaskTide').collection('Jobs');
+
+    app.get('/jobs', async (req, res) => {
+        const result = await jobcollection.find().toArray()
+        res.send(result)
+    })
+  } finally {
+//await deleted
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port,() => console.log(`server running on ${port}`))
 
