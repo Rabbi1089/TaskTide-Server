d36ceb521593358970a95e0d54fade48aa1a3d5d
Fazla Rabbi <155217851+Rabbi1089@users.noreply.github.com>
2024-10-19 11:56:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://solosphere.web.app',
+    'https://tasktide-d2121.web.app/',
   ],
   credentials: true,
   optionSuccessStatus: 200,
